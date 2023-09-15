9a975d27a6c357d361c1d36ca4971a41b1a0de2b
khj951213 <55070877+khj951213@users.noreply.github.com>
2023-09-15 12:35:37 +1000

    temp commit

---
+++ dotnet-socket-client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+var ipAddress = ipHostInfo.AddressList[0];
+var ipEndPoint = new IPEndPoint(ipAddress, 11000);
+
+using Socket client = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+await client.ConnectAsync(ipEndPoint);
+while (true)
+{
+    // Send message.
+    var message = Console.ReadLine();
+    var eom = "<|EOM|>";
+    var messageBytes = Encoding.UTF8.GetBytes(message + eom);
+    _ = await client.SendAsync(messageBytes, SocketFlags.None);
+    Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+    // Receive ack.
+    var buffer = new byte[1_024];
+    var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+    if (response == "<|ACK|>")
+    {
+        Console.WriteLine(
+            $"Socket client received acknowledgment: \"{response}\"");
+    }
+    // Sample output:
+    //     Socket client sent message: "Hi friends 👋!<|EOM|>"
+    //     Socket client received acknowledgment: "<|ACK|>"
+}
---
+++ dotnet-socket-server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+var ipAddress = ipHostInfo.AddressList[0];
+var ipEndPoint = new IPEndPoint(ipAddress, 11000);
+
+using var listener = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+var handler = await listener.AcceptAsync();
+
+while (true)
+{
+    try
+    {
+        // receive message
+        var buffer = new byte[1024];
+        var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+        var response = Encoding.UTF8.GetString(buffer, 0, received);
+        var eom = "<|EOM|>";
+        if (response.IndexOf(eom) > -1)
+        {
+            Console.WriteLine($"Socket server receive message: {response.Replace(eom, "")}");
+            var ackMessage = "<|ACK|>";
+            var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+            await handler.SendAsync(echoBytes, 0);
+            Console.WriteLine($"Socket server sent acknowledgement: {ackMessage}");
+        }
+    }
+    catch (Exception ex)
+    {
+
+    }
+}
